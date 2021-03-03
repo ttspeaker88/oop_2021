c16ada12f88b1c367d724bf27fe6c22c544a6bcb
ttspeaker88 <ttspeaker88@gmail.com>
2021-03-03 15:31:20 +0100

    új könyv objektum létrehozása 04_02 feladatban

--- 04_02/src/Konyv/KonyvProba.java
+++ 04_02/src/Konyv/KonyvProba.java
@@ -11,6 +11,15 @@ public class KonyvProba {
 		k1.setSzerzo("En");
 
 		System.out.println(k1.toString());
+		
+		Konyv k2 = new Konyv();
+
+		k2.setAr(2200);
+		k2.setCim("Nemtudom2");
+		k2.setEv(2021);
+		k2.setSzerzo("En2");
+
+		System.out.println(k2.toString());
 
 	}
 
